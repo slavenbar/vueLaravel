81a0ba3ffe8f2d1707383cf42937c8928b04c55c
slavenbar <gazflair3d@gmail.com>
2022-09-28 14:22:33 +0300

    Laravel vs Vue

---
+++ resources/js/components/testComponent.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="container">
+    <h1>Hello Laravel + Vue</h1>
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Example Component</div>
+
+                    <div class="card-body">
+                        I'm an example component.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
